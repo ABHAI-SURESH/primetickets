97aca268e7a98628697c4182a193ca512fc705f2
ABHAI-SURESH <abhaisuresh12@gmail.com>
2026-05-13 20:10:49 +0530

    changes app style

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 import 'app_colors.dart';
 
 class AppTextStyles {
-  static const TextStyle locationLabel = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.w500,
-    color: AppColors.secondaryText,
-  );
-
   static const TextStyle locationName = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w700,
